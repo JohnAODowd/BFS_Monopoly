1862abaef6431c66795a8f7d9717b4ae2ffd19ab
Fergal Eccles <fergal.j.eccles@gmail.com>
2017-02-22 12:57:16 +0000

    Added base HTML/CSS/JQuery for project.
    Seperate HTML file for each page - Homepage, Lobby, Game
    Homepage uses index.scss/css
    Lobby and game us game.scss/css for game related styling
    
    'app.js' is a jquery file, should be used for forms and general house keeping for scripts
    'lobby.js' is a jquery file, used for forming game lobby and sending lobby resuests to server, as well as handling initial JSON

---
+++ Client/js/app.js
@@ -0,0 +1,14 @@
+(function($){
+	$(function(){
+
+		var canvas = document.getElementById("game-board")
+		var ctx = canvas.getContext("2d");
+
+		canvas.width = 934;
+		canvas.height = 622;
+
+		ctx.fillRect(0,0,520,520);
+
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space

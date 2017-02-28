cfb7ddf0374e69c3adec2da933849989d906ab1e
Dan Delaney <DanDelaney@users.noreply.github.com>
2017-02-28 09:43:27 +0000

    Update initialiseGame.py

--- Server/monopoly/initialiseGame.py
+++ Server/monopoly/initialiseGame.py
@@ -5,11 +5,25 @@ def loadJSON(filename):
         json_data = json.load(json_file)
         return json_data[filename]
 
+def keyStringtoInt(dictionary):
+    # convert all top level dict string keys to ints
+    # eg {"0":"abc} -> {0:"abc"}
+    return {int(k):v for k,v in dictionary.items()}
+
 def getInitialBoard():
-    return loadJSON("board")
+    # return the contents of board.json as a python dict,
+    # with (JSON required) string keys converted to int
+    return keyStringtoInt(loadJSON("board"))
+
+def getCards():
+    # return contents of cards.json as python dict
+    cards = loadJSON("cards")
+    chance = keyStringtoInt(cards['Chance'])
+    communityChest = keyStringtoInt(cards['CommunityChest'])
+    return {"Chance": chance, "CommunityChest" : communityChest}
+
 def getFigurines():
     return ["HAT", "CAR", "THIMBLE", "IRON", "BOOT", "SHIP", "DOG", "WHEELBARROW"]
 def getProperties():
     return loadJSON("properties")
-def getCards():
-    return loadJSON("cards")
+

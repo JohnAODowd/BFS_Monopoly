631fff4341380ad2351a2994a40f5ca2c4e66c56
Fergal Eccles <fergal.j.eccles@gmail.com>
2017-02-28 15:48:34 +0000

    Added JQuery processing of data for a HOST request

--- Client/js/app.js
+++ Client/js/app.js
@@ -1,31 +1,40 @@
 (function($){
 	$(function(){
-			// Game Variables
-			var game_id;
-			var last_activity;
-			var turn;
-			var figurines;
-			var type;
-			var status;
-			var playersNo;
-			// player variables
-			// public
-			var player_name;
-			var player_GOOJF
-			var player_number
-			var player_position
-			var player_properties;
-			//private
-			var playerID;
-			var player_balance;
+		// Game Variables
+		var game_id;
+		var last_activity;
+		var game_turn;
+		var figurines;
+		var game_type;
+		var game_status;
+		var playersNo;
+		// player variables
+		// public
+		var player_name;
+		var player_GOOJF
+		var player_number
+		var player_position
+		var player_properties;
+		// private
+		var playerID;
+		var player_balance;
+		// player list object
+		var player_list;
+		var player_keys;
 
 
 		$(document).ready(function(){
+			/*	=========
+				Core Logic
+				=========	*/
 
 			hostGame();
 
 		});
 
+		/*	=========
+			Functions
+			=========	*/	
 		function joinGame() {
 			console.log("Join Game!")
 
@@ -35,14 +44,14 @@
 			console.log("Host Game!");
 
 			// game variables
-			 console.log('game_variables');
+			console.log('game_variables');
 			game_variables = init['game'];
 			game_id = game_variables['gID']; console.log('GID:' + game_id);
 			last_activity = game_variables['lastActivity']; console.log('last_activity:'+last_activity);
-			turn = game_variables['turn']; console.log('turn:' + turn);
+			game_turn = game_variables['turn']; console.log('turn:' + game_turn);
 			figurines = game_variables['figurines']; console.log('figurines:'+figurines);
-			type = game_variables['type']; console.log('type:'+type);
-			status = game_variables['status']; console.log('status:'+status);
+			game_type = game_variables['type']; console.log('type:'+game_type);
+			game_status = game_variables['status']; console.log('status:'+game_status);
 			playersNo = game_variables['playersNo']; console.log('playersNo:'+playersNo);
 			// player variables
 			console.log('player variables');
@@ -58,7 +67,19 @@
 			player_balance = playerOBJ['money']; console.log('player_balance:'+playerID);
 			// dictionary of players (inclds this player data, we'll check for that later)
 
-
+			// player list 
+			console.log('Player list');
+			player_list = init['players'];
+			player_keys = [];
+			$.each(player_list, function(key, value) {
+				player_keys.push(key);
+			});
+			for (var x=0; x < player_keys.length; x++){
+				console.log(player_keys[x]);
+				$.each(player_list[player_keys[x]], function(key, value){
+					console.log(key + ':' + value);
+				});
+			}
 
 		}
 
@@ -80,5 +101,5 @@
 			img.src = "assets/game_assets/lobby/monopoly-test-background.png";
 		}
 
-  }); // end of document ready
+  	}); // end of document ready
 })(jQuery); // end of jQuery name space

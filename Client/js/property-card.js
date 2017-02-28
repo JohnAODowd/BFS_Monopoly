b954a20b128b1872340cf30e199d6cd34b590c6c
JohnAODowd <114420852@umail.ucc.ie>
2017-02-28 06:54:47 +0000

    test-card.html displays all properties

--- Client/js/property-card.js
+++ Client/js/property-card.js
@@ -1,6 +1,3 @@
-var c = document.getElementById("canvasProperty");
-var ctx = c.getContext("2d");
-
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.open('GET', "../Server/monopoly/properties.json", true);
 xmlhttp.onreadystatechange = function() {
@@ -8,6 +5,15 @@ xmlhttp.onreadystatechange = function() {
         if(xmlhttp.status == 200) {
             var json = JSON.parse(xmlhttp.responseText);
             console.log(json);
+
+            var properties = json.properties;
+            for(var i in properties)
+            {
+
+            console.log(properties[i]);
+
+            var c = document.getElementById(properties[i]['name']);
+			var ctx = c.getContext("2d");
 
 			// Border
 			ctx.beginPath();
@@ -23,26 +29,26 @@ xmlhttp.onreadystatechange = function() {
 			// Colour 
 			ctx.beginPath();
 			ctx.rect(10,10,120,30);
-			ctx.fillStyle = json['properties']['brown1']['colourHex'];
+			ctx.fillStyle = properties[i]['colourHex'];
 			ctx.fill();
 
 			// Setup Title
 			ctx.font = "12px Arial"; 
 			ctx.fillStyle = "black";
 			ctx.textAlign = "center";
-			ctx.fillText(json['properties']['brown1']['name'], 70, 35);
+			ctx.fillText(properties[i]['name'], 70, 35);
 
 			// Setup Title / Rent
 			ctx.font = "10px Arial"; 
-			var rentVal = json['properties']['brown1']['rent'].toString();
+			var rentVal = properties[i]['rent'].toString();
 			ctx.fillText( "Rent $".concat(rentVal), 70, 55);
 			ctx.fillText("Title Deed", 70, 20);
 
 			// Setup bottom of the card
-			var hotelVal = json['properties']['brown1']['hotel'].toString();
+			var hotelVal = properties[i]['hotel'].toString();
 			ctx.fillText( "With HOTEL $".concat(hotelVal), 70, 115);
 
-			var mortVal = json['properties']['brown1']['mortgage'].toString();
+			var mortVal = properties[i]['mortgage'].toString();
 			ctx.fillText( "Mortgage Value $".concat(mortVal), 70, 130);
 
 			/*
@@ -59,11 +65,11 @@ xmlhttp.onreadystatechange = function() {
 			ctx.fillText( "With 4 Houses ", 10, 100);
 
 			ctx.textAlign = "right";
-			ctx.fillText( json['properties']['brown1']['house1'], 130, 70);
-			ctx.fillText( json['properties']['brown1']['house2'], 130, 80);
-			ctx.fillText( json['properties']['brown1']['house3'], 130, 90);
-			ctx.fillText( json['properties']['brown1']['house4'], 130, 100);
-
+			ctx.fillText( properties[i]['house1'], 130, 70);
+			ctx.fillText( properties[i]['house2'], 130, 80);
+			ctx.fillText( properties[i]['house3'], 130, 90);
+			ctx.fillText( properties[i]['house4'], 130, 100);
+			}
          }
     }
 };
